ece80cb73f9ffa05de93184aa6673fa7342cb0d2
Woody <syg1138@gmail.com>
2025-08-06 23:12:45 +0900

    tag.js typescript 변경

--- gatsby-node.ts
+++ gatsby-node.ts
@@ -10,7 +10,7 @@ import { createFilePath } from 'gatsby-source-filesystem'
 
 // Define the template for blog post
 const blogPost = path.resolve(`./src/templates/blog-post.tsx`)
-const tagTemplate = path.resolve(`./src/templates/tag.js`)
+const tagTemplate = path.resolve(`./src/templates/tag.tsx`)
 const tagsTemplate = path.resolve(`./src/templates/tags.js`)
 
 export const createPages: GatsbyNode['createPages'] = async ({ graphql, actions, reporter }) => {
--- src/templates/tag.tsx
+++ src/templates/tag.tsx
@@ -1,10 +1,15 @@
+/**
+ * TagPage - 특정 태그에 해당하는 블로그 포스트 목록을 보여주는 페이지 템플릿
+ * URL: /tags/{태그명} (예: /tags/javascript)
+ * 기능: 해당 태그가 붙은 모든 포스트를 날짜순으로 정렬하여 표시
+ */
 import * as React from "react"
-import { Link, graphql } from "gatsby"
+import { Link, graphql, PageProps } from "gatsby"
 
 import Layout from "../components/Layout"
 import Seo from "../components/Seo"
 
-const TagPage = ({ data, location, pageContext }) => {
+const TagPage = ({ data, location, pageContext }: PageProps<any, { tag: string }>) => {
   const siteTitle = data.site.siteMetadata?.title || `Title`
   const posts = data.allMarkdownRemark.nodes
   const { tag } = pageContext
